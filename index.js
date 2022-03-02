a5cdef6a9b1dcf268f1d56216162e4528e258d7c
bigppfrontdev <rauh.benu@gmail.com>
2022-03-02 13:46:00 +0200

    masin

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-
+app.use(express.static('build'))
 const cors = require('cors')
 
 app.use(cors())
